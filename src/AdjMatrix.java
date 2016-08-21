d6f0ecee0960b2da2af8985853b80a2fc27d4276
rmit-s3569594-Kan-Bu <s3569594@student.rmit.edu.au>
2016-08-21 18:15:15 +1000

    Verified by NK only, useless printings not removed yet

--- src/AdjMatrix.java
+++ src/AdjMatrix.java
@@ -5,14 +5,14 @@ import java.util.Map.Entry;
 
 /**
  * Adjacency matrix implementation for the FriendshipGraph interface.
- * 
+ *
  * Your task is to complete the implementation of this class.  You may add methods, but ensure your modified class compiles and runs.
  *
  * @author Jeffrey Chan, 2016.
  */
 public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 {
-	
+
 	private int arraySize;
 	private NKList list = new NKList();
 	private int[][] friends;
@@ -20,8 +20,8 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 	public static int resizeFactor = 2;
 	private HashMap<String, Integer> distMap = new HashMap<String, Integer>();
 	private NKList visitedList = new NKList();
-	
-	
+
+
 	/**
 	 * Constructs empty graph.
 	 */
@@ -31,8 +31,8 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     	friends = new int[arraySize][arraySize];
     	totalPeople = 0;
     } // end of AdjMatrix()
-    
-    
+
+
     public void addVertex(T vertLabel) {
     	int index;
     	// check if the array overflow or not
@@ -44,8 +44,8 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     		// in the condition that no overflow
     		index = totalPeople;
     	}
-    		
-    	
+
+
     	// register new person
     	for(int i = 0; i < index; i++)
     		friends[index][i] = 0;
@@ -55,203 +55,214 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     	// update the total people
     	totalPeople ++;
     } // end of addVertex()
-	
-    
+
+
     public void addEdge(T srcLabel, T tarLabel) {
-    	   
+
     	//??check if these two people exist ?? Try-catch-throw??
-    	
+
     	if(!list.alreadyExist((String)srcLabel))
     		throw new IllegalArgumentException("The first person does not exist! Please add the person first!");
-    		
-    		
-    	
+
+
+
     	if(!list.alreadyExist((String)tarLabel)){
     		throw new IllegalArgumentException("The last person does not exist! Please add the person first!");
     	}
-    	
+
     	//find and update the srcLabel's friends list
     	int srcIndex = list.getIndex((String)srcLabel);
     	int tarIndex = list.getIndex((String)tarLabel);
     	friends[srcIndex][tarIndex] = 1;
     	friends[tarIndex][srcIndex] = 1;
     } // end of addEdge()
-	
+
 
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
-        
+
         // ?? check if the vertLabel exists or not??
         if(!list.alreadyExist((String)vertLabel)){
         	throw new IllegalArgumentException("The person does not exist! Please add the person first!");
     	}
-        
+
         // find the index in the array
         int index = list.getIndex((String)vertLabel);
-        
-        // find that specific row in the matrix  
+
+        // find that specific row in the matrix
         for(int i = 0; i < friends[index].length; i++){
-        	if(friends[index][i] == 1){
+        	if(friends[index][i] == 1 && index != i){
         		neighbours.add((T)list.getVertice(i));
+        		System.out.println(list.getVertice(i));
         	}
         }
-        
+
         return neighbours;
     } // end of neighbours()
-    
-    
+
+
     public void removeVertex(T vertLabel) {
     	// ?? check if the vertLabel exists or not?? try-catch??
 	   	 if(!list.alreadyExist((String)vertLabel)){
 	   		throw new IllegalArgumentException("The person does not exist!");
 	    	}
-	   	 	   	 
+
 	   	 // get the index of the vertLabel
 	   	 int index = list.getIndex((String)vertLabel);
-	   	 
-	   	 // up every row below the index 
+
+	   	 // up every row below the index
 	   	 for(int i = index; i < totalPeople - 1; i++){
 	   		 for(int j = 0; j < totalPeople; j++){
-	   			 friends[i][j] = friends[i][j+1]; 
+	   			 friends[i][j] = friends[i][j+1];
 	   		 }
 	   	 }
 	   	 // left every column on the right of the verLabel
 	   	 for(int j = index; j < totalPeople - 1; j++){
 	   		 for(int i = 0; i < totalPeople; i++){
-	   			 friends[i][j] = friends[i+1][j]; 
+	   			 friends[i][j] = friends[i+1][j];
 	   		 }
 	   	 }
-	   
+
 	   	 // clear the last row and column
 	   	 for(int i = 0; i < totalPeople; i++){
 	   		 friends[totalPeople-1][i] = 0;
 	   		 friends[i][totalPeople-1] =0;
 	   	 }
-	   	 
-	   	 // delete the vertice from the NKList 
+
+	   	 // delete the vertice from the NKList
 	   	 list.deleteVertice((String)vertLabel);
-	
+
 	   	 // update total people number
 	   	 totalPeople --;
-	    
+
 
 	   	 // downsize if the current size if less than half full
 	   	 if(totalPeople  < Math.round(friends.length/2)){
+	   		 System.out.println("Maths: " + Math.round(friends.length/2));
 	   		 downSize(this.resizeFactor);
 	   	 }
 
-    	
+
     } // end of removeVertex()
-	
-    
+
+
     public void removeEdge(T srcLabel, T tarLabel) {
-    	
+
     	//??check if these two people exist and the edge existed? ?? Try-catch-throw??
     	if(!list.alreadyExist((String)srcLabel)){
     		throw new IllegalArgumentException("The first person does not exist! Please add the person first!");
     	}
-    	
+
     	if(!list.alreadyExist((String)tarLabel)){
     		throw new IllegalArgumentException("The last person does not exist! Please add the person first!");
     	}
-    	 
+
     	// remove the edge on both sides
     	int scrIndex = list.getIndex((String)srcLabel);
     	int tarIndex = list.getIndex((String)tarLabel);
-    	
+
     	friends[scrIndex][tarIndex] = 0;
     	friends[tarIndex][scrIndex] = 0;
-    	
+
     } // end of removeEdges()
-	
-    
+
+
     public void printVertices(PrintWriter os) {
     	for(int i = 0; i< totalPeople; i++){
-    		os.print(list.getVertice(i) + '\t');
+    		os.println(list.getVertice(i) + '\t');
     	}
-    	
-    	os.print('\n');
-    	
+
     } // end of printVertices()
-	
-    
+
+
     public void printEdges(PrintWriter os) {
         for(int i = 0; i < totalPeople; i++){
         	for(int j =0; j < totalPeople; j++){
         		if((friends[i][j] == 1) && (i != j)){
         			os.println(list.getVertice(i) +'\t'+ list.getVertice(j));
         		}
-        			
+
         	}
         }
     } // end of printEdges()
-    
-    
+
+
     public int shortestPathDistance(T vertLabel1, T vertLabel2) {
     	//??check if these two people exist and the edge existed? ?? Try-catch-throw??
     	if(!list.alreadyExist((String)vertLabel1)){
     		throw new IllegalArgumentException("The first person does not exist! Please add the person first!");
     	}
-    	
+
     	if(!list.alreadyExist((String)vertLabel2)){
     		throw new IllegalArgumentException("The last person does not exist! Please add the person first!");
     	}
-    	
+
     	int disconnectedDist = 0;
-    	
+    	distMap.clear();
+    	visitedList.clear();
+
     	if(vertLabel1.equals(vertLabel2)){
     		return disconnectedDist;
     	}
-    	
+
     	// initialize with the starting point
+    	if (distMap.isEmpty()) {
+    		System.out.println("Map is empty.");
+    	}
     	distMap.put((String)vertLabel1, disconnectedDist);
     	visitedList.addVertice((String) vertLabel1);
     	disconnectedDist = NKbfsMatrix((String) vertLabel1, (String)vertLabel2);
-    	
-    	
-    	
+
+
+   // 	distMap.
         // if we reach this point, source and target are disconnected
-        return disconnectedDist;    	
+        return disconnectedDist;
     } // end of shortestPathDistance()
-    
-    
+
+
     public int NKbfsMatrix(String startPerson, String targPerson){
-    	
-    	
+
     	int currentDist = distMap.get(startPerson);
-    	
+    	System.out.println("Current distance: (should be 0)" + currentDist);
+
     	// check if startPerson is the target person
     	if(startPerson.equals(targPerson)){
     		return currentDist;
     	}
-    	
-    	
+
+
     	ArrayList<T> friendsNearby = neighbours((T)startPerson);
     	for(int i = 0; i < friendsNearby.size(); i++){
     		String tempFriend = (String)friendsNearby.get(i);
     		// check if it is visited
     		if(!visitedList.alreadyExist(tempFriend)){
-    			// if not visited, add to list and map 
-    			visitedList.addVertice(tempFriend);
-    			distMap.put(tempFriend, currentDist+1);
+    			// if not visited, add to list and map
+    			if(tempFriend.equals(targPerson))
+    				return (currentDist +1);
+    			else{
+    				visitedList.addVertice(tempFriend);
+        			distMap.put(tempFriend, currentDist+1);
+    			}
     		}
     	}
-    	
+
     	// find the next person to recurse in the visitedList
     	Node currentNode = visitedList.nkHead;
     	while(!currentNode.getValue().equals(startPerson)){
     		currentNode = currentNode.getNext();
     	}
-    	
+
     	// if targPerson cannot be found throughout the list, return -1
-    	if(currentNode.getNext() == null)
+    	if(currentNode.getNext() == null) {
+    		System.out.println("Can't find you!");
     		return -1;
-    	else 
-    		return NKbfsMatrix(currentNode.getNext().getValue(), targPerson); 
+    	}
+    	else
+    		return NKbfsMatrix(currentNode.getNext().getValue(), targPerson);
     }
-    
-    
-    
+
+
+
     public void expand(int factor){
     	arraySize = arraySize * factor;
     	int[][] largerFriends = new int[arraySize*factor][arraySize*factor];
@@ -262,16 +273,17 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
     	}
     	friends = largerFriends;
     }
-    
+
     public void downSize(int factor){
+    	System.out.println("original Arraysize: " + arraySize);
     	arraySize = Math.round(arraySize/factor);
     	int[][] newArray = new int[arraySize][arraySize];
-    	for(int i = 0; i < friends.length; i++){
-    		for(int j = 0; j < friends[i].length; j++){
-    			newArray[i][j] = friends[i][j]; 
+    	for(int i = 0; i < newArray.length; i++){
+    		for(int j = 0; j < newArray[i].length; j++){
+    			newArray[i][j] = friends[i][j];
     		}
     	}
     	friends = newArray;
     }
-    
+
 } // end of class AdjMatrix
--- src/NKList.java
+++ src/NKList.java
@@ -2,16 +2,16 @@ import java.lang.reflect.Array;
 
 
 public class NKList{
-	
-	protected Node nkHead;	
+
+	protected Node nkHead;
 	protected int nkLength;
-	
+
 	// constructor
 	public NKList(){
 		nkHead = null;
 		nkLength = 0;
 	}
-	
+
 	public NKList(Node head){
 		nkHead = head;
 		nkLength = 0;
@@ -21,41 +21,41 @@ public class NKList{
 			currentNode = currentNode.getNext();
 		}
 	}
-	
+
 	// accessor
 	public int getLength(){
 		return nkLength;
 	}
-	
+
 	// get the node at the specified index, which starts at 0.
 	public String getVertice(int index){
 		// verify the validity of the index
 		if(index < 0 || index > nkLength){
 			return null;
 		}
-		
+
 		Node currentNode = nkHead;
 		// move to the node before the specified
 		for(int i = 0; i < index; i++){
 			currentNode = currentNode.getNext();
 		}
-		
+
 		if(currentNode == null)
 			return null;
 		else
 			return currentNode.getValue();
 	}
-	
-	
-	
+
+
+
 	// get the index of a specific node, which starts at 0
 	public int getIndex(String vertice){
 		int index = -1;
 		boolean found = false;
-		
+
 		Node currentNode = nkHead;
 		while(currentNode != null){
-			if(currentNode.getValue() == vertice){
+			if(currentNode.getValue().equals(vertice)){
 				index ++;
 				found = true;
 				break;
@@ -64,22 +64,22 @@ public class NKList{
 				index ++;
 			}
 		}
-		
+
 		if(found){
 			return index;
 		}else{
 			index = -1;
 			return index;
 		}
-		
+
 	}
-	
+
 	// add a node, no duplicates
 	public void addVertice(String newValue){
 		// check duplicates
 		if(alreadyExist(newValue))
 			return;
-		
+
 		// initialize the head with the new value if the list is vacant
 		if(nkHead == null){
 			nkHead = new Node(newValue);
@@ -95,37 +95,37 @@ public class NKList{
 		// update the length
 	    nkLength++;
 	}
-	
+
 	// add a node to a specific index, index start at 0
 	public void addVertice(String newValue, int index){
 		// check index's validity
-		if(index < 0 || index > nkLength) 
+		if(index < 0 || index > nkLength)
 			return;
-		
+
 		// check duplicates
 		if(alreadyExist(newValue))
 			return;
-		
+
 		Node newNode = new Node(newValue);
 		Node currentNode = nkHead;
-		
+
 		// initialize the head with the new value if the list is vacant
 		if(currentNode == null){
 			currentNode = new Node(newValue);
 	    }else{
-	   		
+
 	   		// if it is the head to be changed
 	    	if(index == 0){
 	    		newNode.setNext(currentNode);
 	    		nkHead = newNode;
 	    	}
-	    		
+
 	    	else{
 	    		// move to the specified index of the list
 	    		for(int i = 0; i < index - 1; i++){
 		    		currentNode = currentNode.getNext();
 		    	}
-		    	// add the new node before the existing 
+		    	// add the new node before the existing
 	    		if(currentNode.getNext() != null){
 	    			// not add to the last
 	    			newNode.setNext(currentNode.getNext());
@@ -134,53 +134,53 @@ public class NKList{
 	    			// add to the last
 	    			currentNode.setNext(newNode);
 	    		}
-		    	
+
 	    	}
-	    		    	
+
 	    }
 		// update the length
 	    nkLength++;
 	}
-	
+
 	// delete the node with specific name
 	public void deleteVertice(String targPerson){
 		Node currentNode = nkHead;
-		
+
 		// if the head is the one to be deleted
-		if(nkHead.getValue() == targPerson){
+		if(nkHead.getValue().equals(targPerson)){
 			nkHead = nkHead.getNext();
 			nkLength --;
 			return;
 		}
-		
+
 		// to find the target in the middle of the list
 		while(currentNode.getNext().getNext() != null){
-			if(currentNode.getNext().getValue() == targPerson){  // ?? is it the right way to compare the value??
+			if(currentNode.getNext().getValue().equals(targPerson)){  // ?? is it the right way to compare the value??
 				currentNode.setNext(currentNode.getNext().getNext());
 				nkLength--;
 				return;
 			}
 			currentNode = currentNode.getNext();
 		}
-		
+
 		// if the last is the one to be deleted
-		if(currentNode.getNext().getValue() == targPerson){
+		if(currentNode.getNext().getValue().equals(targPerson)){
 			currentNode.setNext(null);
 			nkLength--;
 		}
-		
+
 	}
-	
+
 	// delete the node at the specified index; index starts at 0
 	public void deleteVertice(int index){
 		// check index's validity
-		if(index < 0 || index >= nkLength) 
+		if(index < 0 || index >= nkLength)
 			return;
-		
+
 		Node currentNode = nkHead;
-		
+
 		// remove the head if there is only one node
-		if(index == 1){
+		if(index == 0){
 			nkHead = nkHead.getNext();
 		}else{
 			// move to the node just before the specified
@@ -192,13 +192,21 @@ public class NKList{
 		}
 		nkLength--;
 	}
-	
+
+	// clear everything in NKList
+	public void clear(){
+		int original = getLength();
+		for(int i = 0; i < original; i++){
+			deleteVertice(0);
+		}
+	}
+
 	// check the existing node values to avoid duplicates
 	public boolean alreadyExist(String newValue){
 		Node currentNode = nkHead;
 		while(currentNode != null){
-			if(currentNode.getValue() == newValue){  
-				return true;  
+			if(currentNode.getValue().equals(newValue)){
+				return true;
 			}else
 				currentNode = currentNode.getNext();
 		}
