6370de4c1f1f8b28b7d2b1c2dfebd36d9b951d7a
rmit-s3569594-Kan-Bu <s3569594@student.rmit.edu.au>
2016-08-28 18:53:27 +1000

    Kan edited the reset

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Random;
@@ -39,34 +40,48 @@ public class DataGenerator {
 
 			switch (typechoice) {
 			case "AV":
-				System.out.println("Input the number of vertices to be generated");
-				int verticesadd = reader.nextInt();
+				
+				System.out.println("Input the starting number of relationships to be generated");
+				int starting = reader.nextInt();
+				System.out.println("Input the ending number of relationships to be generated");
+				int ending = reader.nextInt();
+				int verticesadd = ending - starting;
+				
 				for (int x = 4039; x < verticesadd + 4039; x++) {
-					writer.println("AV " + x);
-					System.out.println("AV " + x);
+					writer.println("AV " + (x + starting));
+					System.out.println("AV " + (x + starting));
 				}
 				writer.close();
 				reader.close();
 				break;
 			case "AE":
-				System.out.println("Input the number of relationships to be generated");
-				int tobeadded = reader.nextInt();
+				System.out.println("Input the number of edges to be generated");
+				int tobeAdded = reader.nextInt();
 				int counter = 0;
-				while (counter < tobeadded) {
+				int[][] newDataSet = new int[tobeAdded][2];
+				while (counter < tobeAdded) {
 					Random ran = new Random();
 					int vertex1 = ran.nextInt(4039);
 					int vertex2 = ran.nextInt(4039);
 					String p1 = Integer.toString(vertex1);
 					String p2 = Integer.toString(vertex2);
-					if (!p1.equals(p2)) {
-						if (checkFile(filename, p1, p2) == false) {
-							writer.println(vertex1 + " " + vertex2);
-							System.out.println("Found " + p1 + " " + p2);
-							counter++;
+					
+					if (!p1.equals(p2)) { // avoid adding self-linkage
+						if (checkFile(filename, p1, p2) == false) { // avoid adding existing edges in original file
+							if(checkNewDataSet(newDataSet, vertex1, vertex1)){ // avoid adding existing edges in the new collection
+								newDataSet[counter][0] = vertex1;
+								newDataSet[counter][1] = vertex2;
+								counter++;
 							}
 						}
 						else
 							System.err.println("Edge found");
+					}
+				}
+				
+				// write to file
+				for(int i = 0; i < newDataSet.length; i++){
+					writer.println(Integer.toString(newDataSet[i][0]) + " " + Integer.toString(newDataSet[i][1]));
 				}
 				writer.close();
 				break;
@@ -92,23 +107,30 @@ public class DataGenerator {
 				break;
 			case "RE":
 				System.out.println("Input the number of edges to be deleted");
-				int tobedeleted = reader.nextInt();
-				int count = 0;
-				while (count < tobedeleted) {
+				int toBeDeletedE = reader.nextInt();
+				int countE = 0;
+				int[][] newDataCollection = new int[toBeDeletedE][2];
+				while (countE < toBeDeletedE) {
 					Random ran = new Random();
 					int vertex1 = ran.nextInt(4039);
 					int vertex2 = ran.nextInt(4039);
 					String p1 = Integer.toString(vertex1);
 					String p2 = Integer.toString(vertex2);
-					if (!p1.equals(p2)) {
-						if (checkFile(filename, p1, p2) == true) {
-							writer.println("RE " + vertex1 + " " + vertex2);
-							System.out.println("Found " + p1 + " " + p2);
-							count++;
+					if (!p1.equals(p2)) { // check self links
+						if (checkFile(filename, p1, p2) == true) { // check not in the original file
+							if(checkNewDataSet(newDataCollection, vertex1, vertex2)){ // check not in the new generated collection
+								newDataCollection[countE][0] = vertex1;
+								newDataCollection[countE][1] = vertex2;
+								countE++;
+							}
 						}
 						else
 							System.err.println("Not Found");
 					}
+				}
+				
+				for(int i = 0; i < newDataCollection.length; i++){
+					writer.println(Integer.toString(newDataCollection[i][0]) + " " + Integer.toString(newDataCollection[i][1]));
 				}
 				writer.close();
 				reader.close();
@@ -128,7 +150,7 @@ public class DataGenerator {
 				// write the data in files
 				Iterator<Integer> itrN = toBeGeneratedN.iterator();
 				while(itrN.hasNext()){
-					writer.println("RV " + itrN.next().toString());
+					writer.println("N " + itrN.next().toString());
 	            }
 				writer.close();
 				reader.close();
@@ -136,16 +158,24 @@ public class DataGenerator {
 			case "S":
 				System.out.println("Input the number of paths to calculate");
 				int pathstocalculate = reader.nextInt();
+				int[][] newData = new int[pathstocalculate][2];
 				for (int x = 0; x < pathstocalculate; x++) {
 					Random ran = new Random();
 					int vertex1 = ran.nextInt(4039);
 					int vertex2 = ran.nextInt(4039);
 					String p1 = Integer.toString(vertex1);
 					String p2 = Integer.toString(vertex2);
-					if (!p1.equals(p2)) {
-						writer.println("S " + p1 + " " + p2);
+					if (!p1.equals(p2)) {// check if p1 and p2 are the same
+						if(checkNewDataSet(newData, vertex1, vertex2)){ // check if they are covered already in the newly generated
+							newData[x][0] = vertex1;
+							newData[x][1] = vertex2;
 						}
 					}
+				}
+				for(int i = 0; i < newData.length; i++){
+					writer.println(Integer.toString(newData[i][0]) + " " + Integer.toString(newData[i][1]));
+				}
+				
 				reader.close();
 				writer.close();
 				break;
@@ -193,5 +223,28 @@ public class DataGenerator {
 			System.out.println("Cannot open file ");
 		}
 		return false;
+	}
+	
+	public static boolean checkNewDataSet(int[][] newDataSet, int p1, int p2){
+		boolean result = true;
+		Set<Integer> existing  = new HashSet<Integer>();
+		for(int i = 0; i < newDataSet.length; i++){
+			if(newDataSet[i][0] == p1){
+				existing.add(newDataSet[i][1]);
+			}
+			
+			if(newDataSet[i][1] == p1){
+				existing.add(newDataSet[i][0]);
+			}
+		}
+		
+		Iterator<Integer> itr = existing.iterator();
+		while(itr.hasNext()){
+			if(itr.next() == p2){
+				result = false;
+				break;
+			}
+        }
+		return result;
 	}
 }
